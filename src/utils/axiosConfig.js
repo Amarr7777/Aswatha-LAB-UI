722536ebaa630b080f5868bb752cecc3969154ca
Amar G Nath <ambadypradeepcoc@gmail.com>
2025-05-18 14:32:23 +0530

    prod config

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api', 
-  // baseURL: 'https://aswathi-lab-server.onrender.com/api', 
+  // baseURL: 'http://localhost:3000/api', 
+  baseURL: 'https://aswathi-lab-server.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json'
   }
